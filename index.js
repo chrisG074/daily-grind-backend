f39e727576eaee1c29a6f89db0543bfc1c4c72d4
Andre Huang <1355686@student.rocvantwente.nl>
2026-03-10 11:15:16 +0100

    Template

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const { Pool } = require("pg");
 
 const app = express();
 const port = Number(process.env.PORT) || 5000;
+
+app.use(cors());
 
 const databaseUrl = process.env.DATABASE_URL;
 const pool = databaseUrl
